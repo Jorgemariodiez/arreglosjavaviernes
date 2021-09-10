40d25f4fc3688db6780ef2dbb8dfac5288024635
503 <503@503-11.academicos.cesde.edu.co>
2021-09-10 10:14:32 -0500

    final de 10 sep 2021

--- src/arreglos/Arreglos.java
+++ src/arreglos/Arreglos.java
@@ -1,6 +1,7 @@
 
 package arreglos;
 
+import java.util.Scanner;
 import paquete2.Heroe;
 
 
@@ -8,6 +9,38 @@ public class Arreglos {
 
    
     public static void main(String[] args) {
+        //crear arreglos
+        //int edad = 50;
+        Scanner entrada = new Scanner(System.in);
+        
+        int[] numeros = new int[5];
+       /* numeros[0]=10;
+        numeros[1]=20;
+        numeros[2]=30;
+        numeros[3]=40;
+        numeros[4]=50;
+        
+        System.out.println(numeros[0]);
+        System.out.println(numeros[1]);
+        System.out.println(numeros[2]);
+        System.out.println(numeros[3]);
+        System.out.println(numeros[4]);*/
+        System.out.println("--------------------------");
+        
+        for(int i=0; i<numeros.length; i++){
+            System.out.println("Digite un numero: ");
+            numeros[i]=entrada.nextInt();
+                    
+            
+            
+        }
+        
+        for(int i=0; i<numeros.length; i++){
+            System.out.println(numeros[i]);
+            
+        }
+        
+        
         // como se utiliza una clase
      // crear objetos
      //una variable
